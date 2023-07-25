ed45a7ae1460dd82dfdeca81116bb5202f4ee104
kachaev-s <metallian@yandex.ru>
2023-07-25 12:08:30 +0300

    1

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
